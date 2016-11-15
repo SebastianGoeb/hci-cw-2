606bbd7b26ea6c69b760f7b1878bf66a790439af
Matthew Summers <matt@wishf.co.uk>
2016-11-15 22:06:12 +0000

    First pass on uni buildings - created separate JS as with accom

---
+++ buildings.js
@@ -0,0 +1,81 @@
+
+var uni_buildings = {
+        title: 'Uni Buildings',
+        places: [{
+          latLng: {lat: 55.946036, lng: -3.192112},
+          title: 'Forrest Hill',
+          content: '<h3>Forrest Hill</h3><p>This is Forrest Hill. You can find the ITO and IGS here.</p>',
+          subplaces: ['ITO', 'IGS', 'Study area']
+        }, {
+          latLng: {lat: 55.944757, lng: -3.187455},
+          title: 'Informatics Forum',
+          content: '<h3>Informatics Forum</h3><p>This is the Informatics Forum. You can find PhD offices here.</p>',
+          subplaces: ['PhD offices']
+        }, {
+            latLng: {lat: 55.945015, lng: -3.188608},
+            title: 'Teviot Row House',
+            content: '<h3>Teviot Row House</h3><p>This is Teviot Row House, more commonly referred to as Teviot. It is the main student union building on the Central campus</p>',
+            subplaces: ['Food and Drink', 'Event venue']
+        }, {
+            latLng: {lat: 55.946041, lng: -3.188328},
+            title: 'Potterrow',
+            content: '<h3>Potterrow</h3><p>This is Potterrow. It is one of the student union buildings on the Central campus</p>',
+            subplaces: ['Food and Drink', 'Event venue']
+        }, {
+            latLng: {lat: 55.944386, lng: -3.186970},
+            title: 'Appleton Tower',
+            content: '<h3>Appleton Tower</h3><p>This is Appleton Tower. It is one of the primary Informatics buildings on campus, currently under renovation</p>',
+            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
+        }, {
+            latLng: {lat: 55.943199, lng: -3.186585},
+            title: 'David Hume Tower',
+            content: '<h3>David Hume Tower</h3><p>This is David Hume Tower, which contains many lecture theatres, some study space, shops and cafes</p>',
+            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
+        }, {
+            latLng: {lat: 55.942717, lng: -3.189168},
+            title: 'Main Library',
+            content: '<h3>Main Library</h3><p>This is the main library on the Central campus.</p>',
+            subplaces: ['Library']
+        }, {
+            latLng: {lat: 55.943052, lng: -3.187513},
+            title: 'Business School',
+            content: '<h3>Business School</h3><p>This is the Business School</p>',
+            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
+        }, {
+            latLng: {lat: 55.944268, lng: -3.189683},
+            title: 'Hugh Robson Building',
+            content: '<h3>Hugh Robson Building</h3><p>This is the Hugh Robson Building, used primarily by the School of Biomedical Sciences</p>',
+            subplaces: ['Lecture Theatres']
+        }, {
+            latLng: {lat: 55.944214, lng: -3.190810},
+            title: 'Chrystal Macmillan Building',
+            content: '<h3>Chrystal Macmillan Building</h3><p>This is the Chrystal Macmillan Building, used primarily by the School of Social and Political Science</p>',
+            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
+        }, {
+            latLng: {lat: 55.944470, lng: -3.188543},
+            title: '7 George Square',
+            content: '<h3>7 George Square</h3><p>This is 7 George Square, used primarily by the Psychology department</p>',
+            subplaces: ['Lecture Theatres', 'Study area']
+        }, {
+            latLng: {lat: 55.942811, lng: -3.188184},
+            title: 'George Square Lecture Theatre',
+            content: '<h3>George Square Lecture Theatre</h3><p>This is the George Square Lecture Theatre</p>',
+            subplaces: ['Lecture Theatre']
+        }, {
+            latLng: {lat: 55.945772, lng: -3.187645},
+            title: '7 Bristo Square',
+            content: '<h3>7 Bristo Square</h3><p>This is 7 Bristo Square. It is used primarily by the Law school</p>',
+            subplaces: ['Lecture Theatres']
+        }, {
+            latLng: {lat: 55.922466, lng: -3.175340},
+            title: 'King\'s Buildings',
+            content: '<h3>King\'s Buildings</h3><p>This is King\'s Buildings, used primarily by the engineering, maths and science departments</p>',
+            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
+        }, {
+            latLng: {lat: 55.947554, lng: -3.186391},
+            title: 'Old College',
+            content: '<h3>Old College</h3><p>This is Old College, primarily used by the Law school</p>',
+            subplaces: ['Lecture Theatres']
+        },
+        ]
+      }
