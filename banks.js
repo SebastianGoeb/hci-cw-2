5a36db94bfac98c80cce7ba7cbf3afb0e5642a37
Mattias Appelgren <mappelgren@Mattiass-MacBook-Pro-2.local>
2016-11-16 20:55:00 +0000

    added bank description

--- banks.js
+++ banks.js
@@ -1,44 +1,48 @@
 var banks = {
     title: 'Banks',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum laoreet dignissim. Nunc fringilla ornare tristique. Nulla luctus ex erat, ut aliquet tortor consectetur nec. Phasellus et nisl et eros ultricies facilisis ut ut diam. Etiam facilisis id leo ut cursus. Pellentesque commodo tincidunt enim, id aliquam risus porttitor tempor. Donec urna est, venenatis auctor molestie vel, dictum ac orci. Curabitur sollicitudin ultricies tortor. Phasellus sit amet finibus erat. Donec mattis, leo non aliquet euismod, ex felis dapibus dui, vitae feugiat magna lorem ut tellus. Ut vitae elementum enim, quis vestibulum ante. Sed vitae quam semper nisl scelerisque dapibus. Vivamus pulvinar, nibh ut consequat dictum, massa risus auctor libero, ut sagittis velit eros et sapien. Nam vel ex a turpis sollicitudin porttitor. Nunc nec placerat dui.',
+    content: 'Banks in Edinburgh have experience providing accounts to students from all over the world, so opening an account will not be a problem. Simply meet with one of the bank representatives in a local branch and ask to set up an account.\
+    To open an account you will have to bring identification, proof of study, proof of address, and Visa (if applicable).\
+    The safest form of identification will be a passport but EU-identification cards and UK drivers licences are also accepted. A bill containing your address and name is acceptable proof of address. Some banks also accept a bank introduction letter from the university (described below). Proof of study is some proof of your unconditional acceptance to your course. This is either a bank introduction letter found on Euclid (description below) or your UCAS acceptance letter. The exact documents accepted by each bank will vary but will be available on their webpage, so check before you visit.\
+    The University have created a bank introduction letter that can be used at all banks listed below except for NatWest. This letter is sufficient to tell the bank that you are accepted to the course and what your term time address is. It can be found on the University provided service "Euclid". A simple way to find the service is by visiting myed.ed.ac.uk, you should have been provided with an account. Mouse over the "Accounts" tab and click on "EUCLID". In Euclid hit "Documents". The bank introduction letter can be found on this page.\
+    Below follows a list of major banks. Each will have a slightly different student account with different benifits. Click on the buttons to find the bank\'s location and find links to further details. ',
     places: [{
         latLng: {lat: 55.946208, lng: -3.184974},
         title: 'Royal Bank of Scotland',
-        content: 'RBS offers a <a href="http://personal.rbs.co.uk/personal/current-accounts/compare-current-accounts/student-account.html">student account</a> which is available to international students with <a href="http://personal.rbs.co.uk/personal/current-accounts/compare-current-accounts/student-account/student-account-international-students.html">minor modifications</a>.',
+        content: '30 Nicolson St, Edinburgh EH8 9DL<br><br>RBS offers a <a href="http://personal.rbs.co.uk/personal/current-accounts/compare-current-accounts/student-account.html">student account</a> which is available to international students with <a href="http://personal.rbs.co.uk/personal/current-accounts/compare-current-accounts/student-account/student-account-international-students.html">minor modifications</a>.',
         subplaces: ['international student account']
     }, {
         latLng: {lat: 55.953436, lng: -3.190249},
         title: 'Barclays',
-        content: 'Barclays offers student accounts for both domestic and international students. For more  information visit the <a href="http://www.barclays.co.uk/current-accounts/student-accounts">domestic</a> or <a href="http://www.barclays.co.uk/current-accounts/international-student-accounts">international</a> student account page or visit <a href="http://www.barclays.co.uk">www.barclays.co.uk</a>.',
+        content: '2, 10-15 Princes St, Edinburgh EH2 2AN<br><br>Barclays offers student accounts for both domestic and international students. For more  information visit the <a href="http://www.barclays.co.uk/current-accounts/student-accounts">domestic</a> or <a href="http://www.barclays.co.uk/current-accounts/international-student-accounts">international</a> student account page or visit <a href="http://www.barclays.co.uk">www.barclays.co.uk</a>.',
         subplaces: ['international student account']
     }, {
         latLng: {lat: 55.949021, lng: -3.192829},
         title: 'Bank of Scotland',
-        content: 'Bank of Scotland offers a <a href="https://www.bankofscotland.co.uk/bankaccounts/compare-current-accounts/student-account/">student account</a> that is available to any student who has had a permanent UK address for the past 3 years.',
+        content: '300 Lawnmarket, Edinburgh EH1 2PH<br><br>Bank of Scotland offers a <a href="https://www.bankofscotland.co.uk/bankaccounts/compare-current-accounts/student-account/">student account</a> that is available to any student who has had a permanent UK address for the past 3 years.',
         subplaces: []
     },
     {
         latLng: {lat: 55.951117, lng: -3.203165},
         title: 'HSBC',
-        content: 'HSBC offers a <a href="http://www.hsbc.co.uk/1/2/current-accounts/student-bank-account#/overview">student account</a>. They also offer options for international students. Information about these can be found <a href="http://www.hsbc.co.uk/1/2/current-accounts/student-bank-account#/international-students">here</a> and <a href="http://www.hsbc.co.uk/1/2/current-accounts/basic-bank-account">here</a>',
+        content: '118 Princes St, Edinburgh EH2 4AA<br><br>HSBC offers a <a href="http://www.hsbc.co.uk/1/2/current-accounts/student-bank-account#/overview">student account</a>. They also offer options for international students. Information about these can be found <a href="http://www.hsbc.co.uk/1/2/current-accounts/student-bank-account#/international-students">here</a> and <a href="http://www.hsbc.co.uk/1/2/current-accounts/basic-bank-account">here</a>',
         subplaces: ['international account']
     },
     {
         latLng: {lat: 55.945405, lng: -3.186780},
         title: 'Santander',
-        content: 'Santander offer accounts for both <a href="http://www.santander.co.uk/uk/current-accounts/student-graduate-current-accounts/123-student-current-account">domestic</a> and <a href="http://www.santander.co.uk/uk/current-accounts/student-graduate-current-accounts/international-student-current-account">international students</a>',
+        content: 'Potterrow, Edinburgh EH8 9AL<br><br>Santander offer accounts for both <a href="http://www.santander.co.uk/uk/current-accounts/student-graduate-current-accounts/123-student-current-account">domestic</a> and <a href="http://www.santander.co.uk/uk/current-accounts/student-graduate-current-accounts/international-student-current-account">international students</a>',
         subplaces: ['international student account']
     },
     {
         latLng: {lat: 55.953951, lng: -3.195214},
         title: 'NatWest',
-        content: 'NatWest offers a <a href="http://personal.natwest.com/personal/current-accounts/compare-current-accounts/student-account.html">student account</a>. International students can apply for the same account but will not get an overdraft.',
+        content: '8 George St, Edinburgh EH2 2SB<br><br>NatWest offers a <a href="http://personal.natwest.com/personal/current-accounts/compare-current-accounts/student-account.html">student account</a>. International students can apply for the same account but will not get an overdraft.',
         subplaces: ['international student account']
     },
     {
         latLng: {lat: 55.942596, lng: -3.182890},
         title: 'TSB',
-        content: 'TSB offers a <a href="http://www.tsb.co.uk/current-accounts/student-bank-account/">student account</a>. International students can apply for a <a href="http://www.tsb.co.uk/current-accounts/">basic account</a>.',
+        content: '11 Clerk St, Edinburgh EH8 9JH<br><br>TSB offers a <a href="http://www.tsb.co.uk/current-accounts/student-bank-account/">student account</a>. International students can apply for a <a href="http://www.tsb.co.uk/current-accounts/">basic account</a>.',
         subplaces: ['international account']
     }]
 }
