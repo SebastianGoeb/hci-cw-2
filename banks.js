e038378db33236a05c77015ca069223a8d44fb2a
crowec <connie81095@orange.fr>
2016-11-17 14:41:19 +0000

    More Alphabetising

--- banks.js
+++ banks.js
@@ -7,40 +7,36 @@ var banks = {
     <h3>Bank introduction letter</h3><p>The University have created a bank introduction letter that can be used at all banks listed below except for NatWest. This letter is sufficient to tell the bank that you are accepted to the course and what your term time address is. It can be found on the University provided service "Euclid". A simple way to find the service is by visiting <a href="www.myed.ed.ac.uk">MyEd</a>, you should have been provided with an account. Mouse over the "Accounts" tab and click on "EUCLID". In Euclid hit "Documents". The bank introduction letter can be found on this page.</p>\
     <p>Below follows a list of major banks. Each will have a slightly different student account with different benifits. Click on the buttons to find the bank\'s location and find links to further details. </p>',
     places: [{
-        latLng: {lat: 55.946208, lng: -3.184974},
-        title: 'Royal Bank of Scotland',
-        content: '30 Nicolson St, Edinburgh EH8 9DL<br><br>RBS offers a <a href="http://personal.rbs.co.uk/personal/current-accounts/compare-current-accounts/student-account.html">student account</a> which is available to international students with <a href="http://personal.rbs.co.uk/personal/current-accounts/compare-current-accounts/student-account/student-account-international-students.html">minor modifications</a>.',
-        subplaces: ['International student account']
-    }, {
+        latLng: {lat: 55.949021, lng: -3.192829},
+        title: 'Bank of Scotland',
+        content: '300 Lawnmarket, Edinburgh EH1 2PH<br><br>Bank of Scotland offers a <a href="https://www.bankofscotland.co.uk/bankaccounts/compare-current-accounts/student-account/">student account</a> that is available to any student who has had a permanent UK address for the past 3 years.',
+        subplaces: ['Student account']
+    },{
         latLng: {lat: 55.953436, lng: -3.190249},
         title: 'Barclays',
         content: '2, 10-15 Princes St, Edinburgh EH2 2AN<br><br>Barclays offers student accounts for both domestic and international students. For more  information visit the <a href="http://www.barclays.co.uk/current-accounts/student-accounts">domestic</a> or <a href="http://www.barclays.co.uk/current-accounts/international-student-accounts">international</a> student account page or visit <a href="http://www.barclays.co.uk">www.barclays.co.uk</a>.',
         subplaces: ['International student account']
     }, {
-        latLng: {lat: 55.949021, lng: -3.192829},
-        title: 'Bank of Scotland',
-        content: '300 Lawnmarket, Edinburgh EH1 2PH<br><br>Bank of Scotland offers a <a href="https://www.bankofscotland.co.uk/bankaccounts/compare-current-accounts/student-account/">student account</a> that is available to any student who has had a permanent UK address for the past 3 years.',
-        subplaces: ['Student account']
-    },
-    {
         latLng: {lat: 55.951117, lng: -3.203165},
         title: 'HSBC',
         content: '118 Princes St, Edinburgh EH2 4AA<br><br>HSBC offers a <a href="http://www.hsbc.co.uk/1/2/current-accounts/student-bank-account#/overview">student account</a>. They also offer options for international students. Information about these can be found <a href="http://www.hsbc.co.uk/1/2/current-accounts/student-bank-account#/international-students">here</a> and <a href="http://www.hsbc.co.uk/1/2/current-accounts/basic-bank-account">here</a>',
         subplaces: ['International account']
-    },
-    {
-        latLng: {lat: 55.945405, lng: -3.186780},
-        title: 'Santander',
-        content: 'Potterrow, Edinburgh EH8 9AL<br><br>Santander offer accounts for both <a href="http://www.santander.co.uk/uk/current-accounts/student-graduate-current-accounts/123-student-current-account">domestic</a> and <a href="http://www.santander.co.uk/uk/current-accounts/student-graduate-current-accounts/international-student-current-account">international students</a>',
-        subplaces: ['International student account']
-    },
-    {
+    },{
         latLng: {lat: 55.953951, lng: -3.195214},
         title: 'NatWest',
         content: '8 George St, Edinburgh EH2 2SB<br><br>NatWest offers a <a href="http://personal.natwest.com/personal/current-accounts/compare-current-accounts/student-account.html">student account</a>. International students can apply for the same account but will not get an overdraft.',
         subplaces: ['International student account']
-    },
-    {
+    },{
+        latLng: {lat: 55.946208, lng: -3.184974},
+        title: 'Royal Bank of Scotland',
+        content: '30 Nicolson St, Edinburgh EH8 9DL<br><br>RBS offers a <a href="http://personal.rbs.co.uk/personal/current-accounts/compare-current-accounts/student-account.html">student account</a> which is available to international students with <a href="http://personal.rbs.co.uk/personal/current-accounts/compare-current-accounts/student-account/student-account-international-students.html">minor modifications</a>.',
+        subplaces: ['International student account']
+    },{
+        latLng: {lat: 55.945405, lng: -3.186780},
+        title: 'Santander',
+        content: 'Potterrow, Edinburgh EH8 9AL<br><br>Santander offer accounts for both <a href="http://www.santander.co.uk/uk/current-accounts/student-graduate-current-accounts/123-student-current-account">domestic</a> and <a href="http://www.santander.co.uk/uk/current-accounts/student-graduate-current-accounts/international-student-current-account">international students</a>',
+        subplaces: ['International student account']
+    },{
         latLng: {lat: 55.942596, lng: -3.182890},
         title: 'TSB',
         content: '11 Clerk St, Edinburgh EH8 9JH<br><br>TSB offers a <a href="http://www.tsb.co.uk/current-accounts/student-bank-account/">student account</a>. International students can apply for a <a href="http://www.tsb.co.uk/current-accounts/">basic account</a>.',
--- buildings.js
+++ buildings.js
@@ -2,81 +2,81 @@
 var uni_buildings = {
         title: 'Uni Buildings',
         content: 'This is an abbreviated list of some of the more commonly used buildings in the university, focusing on the central campus which has more relevance to informatics students. The Informatics Forum, containing the offices of researchers and PhD students, is not used for lectures or study. This list marks the locations of common lecture venues, though there are many more and it is contingent on course organisers to find one, which may not be on this list. Typically, undergraduate students study in Forrest Hill, with space becoming available in Appleton Tower as renovations progress.',
-        places: [{
-          latLng: {lat: 55.946036, lng: -3.192112},
-          title: 'Forrest Hill',
-          content: 'This is Forrest Hill. You can find the ITO and IGS here.',
-          subplaces: ['ITO', 'IGS', 'Study area']
-        }, {
-          latLng: {lat: 55.944757, lng: -3.187455},
-          title: 'Informatics Forum',
-          content: 'This is the Informatics Forum. You can find PhD offices here.',
-          subplaces: ['PhD offices']
-        }, {
-            latLng: {lat: 55.945015, lng: -3.188608},
-            title: 'Teviot Row House',
-            content: 'This is Teviot Row House, more commonly referred to as Teviot. It is the main student union building on the Central campus.',
-            subplaces: ['Food and Drink', 'Event venue']
-        }, {
-            latLng: {lat: 55.946041, lng: -3.188328},
-            title: 'Potterrow',
-            content: 'This is Potterrow. It is one of the student union buildings on the Central campus.',
-            subplaces: ['Food and Drink', 'Event venue']
-        }, {
-            latLng: {lat: 55.944386, lng: -3.186970},
-            title: 'Appleton Tower',
-            content: 'This is Appleton Tower. It is one of the primary Informatics buildings on campus, currently under renovation.',
-            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
-        }, {
-            latLng: {lat: 55.943199, lng: -3.186585},
-            title: 'David Hume Tower',
-            content: 'This is David Hume Tower, which contains many lecture theatres, some study space, shops and cafes.',
-            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
-        }, {
-            latLng: {lat: 55.942717, lng: -3.189168},
-            title: 'Main Library',
-            content: 'This is the main library on the Central campus.',
-            subplaces: ['Library']
-        }, {
-            latLng: {lat: 55.943052, lng: -3.187513},
-            title: 'Business School',
-            content: 'This is the Business School.',
-            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
-        }, {
-            latLng: {lat: 55.944268, lng: -3.189683},
-            title: 'Hugh Robson Building',
-            content: 'This is the Hugh Robson Building, used primarily by the School of Biomedical Sciences.',
-            subplaces: ['Lecture Theatres']
-        }, {
-            latLng: {lat: 55.944214, lng: -3.190810},
-            title: 'Chrystal Macmillan Building',
-            content: 'This is the Chrystal Macmillan Building, used primarily by the School of Social and Political Science.',
-            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
-        }, {
+        places: [ {
             latLng: {lat: 55.944470, lng: -3.188543},
             title: '7 George Square',
             content: 'This is 7 George Square, used primarily by the Psychology department.',
             subplaces: ['Lecture Theatres', 'Study area']
-        }, {
-            latLng: {lat: 55.942811, lng: -3.188184},
-            title: 'George Square Lecture Theatre',
-            content: 'This is the George Square Lecture Theatre.',
-            subplaces: ['Lecture Theatre']
         }, {
             latLng: {lat: 55.945772, lng: -3.187645},
             title: '7 Bristo Square',
             content: 'This is 7 Bristo Square. It is used primarily by the Law school.',
             subplaces: ['Lecture Theatres']
         }, {
+            latLng: {lat: 55.944386, lng: -3.186970},
+            title: 'Appleton Tower',
+            content: 'This is Appleton Tower. It is one of the primary Informatics buildings on campus, currently under renovation.',
+            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
+        }, {
+            latLng: {lat: 55.943052, lng: -3.187513},
+            title: 'Business School',
+            content: 'This is the Business School.',
+            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
+        },{
+            latLng: {lat: 55.944214, lng: -3.190810},
+            title: 'Chrystal Macmillan Building',
+            content: 'This is the Chrystal Macmillan Building, used primarily by the School of Social and Political Science.',
+            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
+        },{
+            latLng: {lat: 55.943199, lng: -3.186585},
+            title: 'David Hume Tower',
+            content: 'This is David Hume Tower, which contains many lecture theatres, some study space, shops and cafes.',
+            subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
+        }, {
+            latLng: {lat: 55.946036, lng: -3.192112},
+            title: 'Forrest Hill',
+            content: 'This is Forrest Hill. You can find the ITO and IGS here.',
+            subplaces: ['ITO', 'IGS', 'Study area']
+        }, {
+            latLng: {lat: 55.942811, lng: -3.188184},
+            title: 'George Square Lecture Theatre',
+            content: 'This is the George Square Lecture Theatre.',
+            subplaces: ['Lecture Theatre']
+        }, {
+            latLng: {lat: 55.944268, lng: -3.189683},
+            title: 'Hugh Robson Building',
+            content: 'This is the Hugh Robson Building, used primarily by the School of Biomedical Sciences.',
+            subplaces: ['Lecture Theatres']
+        },{
+            latLng: {lat: 55.944757, lng: -3.187455},
+            title: 'Informatics Forum',
+            content: 'This is the Informatics Forum. You can find PhD offices here.',
+            subplaces: ['PhD offices']
+        },{
             latLng: {lat: 55.922466, lng: -3.175340},
             title: 'King\'s Buildings',
             content: 'This is King\'s Buildings, used primarily by the engineering, maths and science departments.',
             subplaces: ['Lecture Theatres', 'Study area', 'Cafe']
+        },  {
+            latLng: {lat: 55.942717, lng: -3.189168},
+            title: 'Main Library',
+            content: 'This is the main library on the Central campus.',
+            subplaces: ['Library']
         }, {
             latLng: {lat: 55.947554, lng: -3.186391},
             title: 'Old College',
             content: 'This is Old College, primarily used by the Law school.',
             subplaces: ['Lecture Theatres']
-        },
+        },  {
+            latLng: {lat: 55.946041, lng: -3.188328},
+            title: 'Potterrow',
+            content: 'This is Potterrow. It is one of the student union buildings on the Central campus.',
+            subplaces: ['Food and Drink', 'Event venue']
+        }, {
+            latLng: {lat: 55.945015, lng: -3.188608},
+            title: 'Teviot Row House',
+            content: 'This is Teviot Row House, more commonly referred to as Teviot. It is the main student union building on the Central campus.',
+            subplaces: ['Food and Drink', 'Event venue']
+        }
         ]
       }
